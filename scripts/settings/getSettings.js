8103af20a189ca5f0bb75670a18d29c9a7e16cc9
Coder TJP <codertjp@gmail.com>
2024-03-28 19:14:59 -0400

    Updating update message

--- scripts/settings/getSettings.js
+++ scripts/settings/getSettings.js
@@ -1,20 +1,8 @@
 let updateHTML = `
 <h1>Welcome to: </h1>
 <h2>{{version}}</h2>
-<h3>We hope you enjoy this update</h3>
+<h3>This update brings API!</h3>
 <h4>Release notes:</h4>
-<li>Added a link list on the right side of the screen, having links related to defly shown directly on the homepage!</li>
-<li>Added color hex codes once your mouse hovers over a color in the skins menu. (This was supposed to be in 1.2.3, but was fixed to show on every browser.)</li>
-<li>Added color highlights on the Elite skin tab as well as text that shows what team won each elite skin.</li>
-<li>Added a new battery page for computers that support battery usage.</li>
-<li>Fixed a bug that drained battery usage on the infoScreen.</li>
-<li>Added a minimap zoom slider! You can change the size via the settings.</li>
-<li>Added a subscript character to the beginning of users' names at the beginning of the description. This displays like a clan tag, and is automatically enabled. You can disable it if wanted.</li>
-<li>Added the ability to inject HTML anywhere on the page!</li>
-<li>Added theme importing! Use your own custom theme or use someone else's to customize the page in ways never seen before!</li>
-<li>Added support for configuration files! Saving the data for the extension to a file is now possible and importing configuration is now possible using this.</li>
-<li>Added the ability to mute players! While in chat, mute them by clicking on their name. There will be a future update making it easier to unmute players after muting them.</li>
-<li>Added advanced censorship with names and text chat.</li>
 `;
 
 function mergeObjects(obj1, obj2) {
