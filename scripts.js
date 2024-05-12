2ff0b0827009f578fb5f13b0e0fc84b61bd6eef9
Coder TJP <codertjp@gmail.com>
2024-05-12 00:08:57 -0400

    Testing things with inspector developer page(failed btw)

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// console.log(chrome);
+
+// function send(message) {
+//     chrome.runtime.sendMessage(message, (response) => {});
+// }
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     //   Find a run code depending on sent message
+//     if (request.message.match("log") !== null) {
+//         document.getElementById('logs').innerHTML = document.getElementById('logs').innerHTML + message.replace("log", "");
+//     }
+//     // Acknowledge message
+//     sendResponse("ok");
+//   });
+//   send('JSON');
+
+
+//   chrome.devtools.panels.elements.createSidebarPanel("My page");
